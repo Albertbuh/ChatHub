6ecb381b7d817f88983aba6e63132560e5fe7554
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-04-18 16:55:59 +0300

    Bug fix

--- client/app/telegram/page.tsx
+++ client/app/telegram/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 import React, { Suspense, useEffect, useState } from "react";
 import MessageList from "../components/messageList/messageList";
-import MessengerContainer from "../components/messengerContainer/messengerContainer";
 import { IMessageInfo } from "../models/dto/IMessageInfo";
 import { GetDialogs } from "../lib/getRequests";
 import Connector from "../utils/singnalR-connector"
 import { IDialogInfo } from "../models/dto/IDialogInfo";
-import DialogContainer from "../components/dialogContainer/dialogContainer";
 import MessengerBase from "./messenger/messengerBase";
 // import DialogUpdateContainer from "../page"
 
