78a92b69b42c0ad5bef78d996f7a03d1534a28bd
albertbuh <albertbuhovets@mail.ru>
2024-05-10 19:34:11 +0300

    fast-login by phone in local storage

--- client/app/telegram/authorization/page.tsx
+++ client/app/telegram/authorization/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Bs123, BsFillKeyFill, BsFillTelephoneFill } from "react-icons/bs";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import Form, { FormProps } from "@/app/components/loginForm/loginForm";
 import { loginRequest } from "@/app/utils/getRequests";
 
@@ -60,6 +60,10 @@ export default function Page() {
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         try {
+            if (currentLoginState.current === LoginState.Phone) {
+                localStorage.setItem("telegram_phone", firstLoginField);
+            }
+
             var isLoggedIn = await loginRequest(
                 "telegram",
                 `http://localhost:5041/api/v1.0/telegram/login?info=${firstLoginField}`,
--- client/middleware.ts
+++ client/middleware.ts
@@ -1,17 +1,37 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { loginRequest } from "./app/utils/getRequests";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     if (request.nextUrl.pathname.startsWith("/telegram")) {
-        if(!request.nextUrl.pathname.endsWith("/authorization") && 
-            !request.cookies.get("telegram_id"))
-            return NextResponse.redirect("http://localhost:44144/telegram/authorization");
-    }
-    if (request.nextUrl.pathname.startsWith("/vkontakte")) {
-        if(!request.nextUrl.pathname.includes("/authorization") &&
-            !request.cookies.get("vkontakte_id"))
-            return NextResponse.redirect("http://localhost:44144/vkontakte/authorization");
+        if (
+            !request.nextUrl.pathname.endsWith("/authorization") &&
+            !request.cookies.get("telegram_id")
+        ) {
+            //check if user already logged
+            var phone = localStorage.getItem("telegram_phone");
+            if (!phone) {
+                return NextResponse.redirect(
+                    "http://localhost:44144/telegram/authorization",
+                );
+            }
+
+            //send request to save data to localStorage and cookies
+            await loginRequest(
+                "telegram",
+                `http://localhost:5041/api/v1.0/telegram/login?info=${phone}`,
+            );
+        }
+    } else if (request.nextUrl.pathname.startsWith("/vkontakte")) {
+        if (
+            !request.nextUrl.pathname.includes("/authorization") &&
+            !request.cookies.get("vkontakte_id")
+        ) {
+            return NextResponse.redirect(
+                "http://localhost:44144/vkontakte/authorization",
+            );
+        }
     }
 }
 
