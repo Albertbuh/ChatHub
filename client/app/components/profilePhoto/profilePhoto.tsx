1aa3362d0a92ca05ed4437e04d0574b2b364c4ba
Intensio <Alexander0aki@gmail.com>
2024-05-29 12:58:13 +0300

    profile photo changed image

--- client/app/components/profilePhoto/profilePhoto.tsx
+++ client/app/components/profilePhoto/profilePhoto.tsx
@@ -1,10 +1,11 @@
+import { useUserStore } from "@/app/realTimeChat/lib/userStore";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface Props {
     src: string;
-    width: number | `${number}`;
-    height: number | `${number}`;
+    width?: number | `${number}`;
+    height?: number | `${number}`;
     alt: string;
     className: string | undefined;
 }
@@ -13,13 +14,15 @@ export default function ProfilePhoto(props: Props) {
     const [profilePath, setProfilePath] = useState(
         props.src,
     );
+    const { currentUser } = useUserStore();
     useEffect(() => {
         setProfilePath(props.src);
     }, [props]);
     
     return (
-        <Image
-            src={profilePath}
+        <img
+            // src={profilePath}
+            src={currentUser?.avatar ?? "/avatars/defaultProfile.jpeg"}
             width={props.width}
             height={props.height}
             alt={props.alt}
