130256b20f722445cb20d6473aa783b32038e59b
Intensio <Alexander0aki@gmail.com>
2024-05-29 12:35:50 +0300

    config cleanup

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,52 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "sun25-1.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "sun25-2.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "pp.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "sun23-2.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "sun23-1.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "sun9-21.userapi.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
